38aa19cbf4a9c3c82d8edb9845930adc921b8253
textSolver34761 <pradonbenjamin@gmail.com>
2022-06-13 19:17:41 +0200

    Home Page gets data from db

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,21 +1,38 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+export default class Welcome extends React.Component {
+  state = {
+    message: [],
+  };
 
-function Welcome() {
+  componentDidMount() {
+    axios.get(`http://localhost:8080`).then((res) => {
+      const message = res.data;
+      this.setState({ message });
+    });
+  }
+  render() {
     return (
-        <div>
-            <Card>
-                <Card.Body>
-                    <Card.Title><h1> Welcome </h1></Card.Title>
-                    <Card.Text>
-                        here you can download our software
-                    </Card.Text>
-                    <Button variant="primary" as={Link} to="/price">Go to pricing</Button>
-                </Card.Body>
-            </Card>
-        </div>
+      <div>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <h1> Welcome </h1>
+            </Card.Title>
+            <Card.Text>
+              {this.state.message.map((message) => (
+                <span key={message.id}>{message.welcome}</span>
+              ))}
+            </Card.Text>
+            <Button variant="primary" as={Link} to="/price">
+              Go to pricing
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
-  
-  export default Welcome;
+}
